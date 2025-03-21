ae054e038fd3680fb1a2afb1a757b9eb5a19a281
Mike-Sells <michael.sells.06@gmail.com>
2025-03-21 17:32:29 +0000

    Added ParticleSystem class functionality

--- src/ParticleSystem.java
+++ src/ParticleSystem.java
@@ -1,1 +1,15 @@
-public class ParticleSystem {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParticleSystem {
+
+    private List<Particle> particles;
+
+    public ParticleSystem() {
+        particles = new ArrayList<>();
+    }
+
+    public void addParticle(Particle p) {
+        particles.add(p);
+    }
+}
