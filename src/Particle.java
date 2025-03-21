6555e81c375520542975e08619879fc2fd57ff79
Mike-Sells <michael.sells.06@gmail.com>
2025-03-21 17:16:03 +0000

    Added Particle class functionality git add src/Particle.java src/Particle.class

--- src/Particle.java
+++ src/Particle.java
@@ -1,1 +1,28 @@
-public class Particle {}
+public class Particle {
+
+    private float xDisplacement, yDisplacement;
+    private float xVelocity, yVelocity;
+    private float radius;
+
+    public Particle(
+        float xDisplacement,
+        float yDisplacement,
+        float xVelocity,
+        float yVelocity,
+        float radius
+    ) {
+        this.xDisplacement = xDisplacement;
+        this.yDisplacement = yDisplacement;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+        this.radius = radius;
+    }
+
+    public void update() {
+        xDisplacement += xVelocity;
+        yDisplacement += yVelocity;
+
+        if (xDisplacement < 0 || xDisplacement > 800) xVelocity = -xVelocity;
+        if (yDisplacement < 0 || yDisplacement > 800) yVelocity = -yVelocity;
+    }
+}
